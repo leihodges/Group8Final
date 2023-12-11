003f583492c136e98f3de2344fa6d20cb53e7a82
leihodges <leilah4114@gmail.com>
2023-12-10 16:22:57 -0800

    fixing the CSS and including ingredients in the JS

--- js/site.js
+++ js/site.js
@@ -1,32 +1,80 @@
-// document.getElementById('next').addEventListener('click', function() {
-//   // Set the new URL for the next page
-//   window.location.href = 'https://leihodges.github.io/Group8Final/page2/index.html';
-// });
+// // Group 8 Final JavaScript
+// // for ART101 Fall 2024
 
+// hide and show js
 $("#home").click(function() {
-  $("#recipeDiv").hide();
+    $("#recipeDiv").hide();
 });
-
+  
 $("#next").click(function() {
-  $("#recipeDiv").show();
+    $("#recipeDiv").show();
 });
 
-// $("#home").click(function() {
-//   $("#recipeDiv").hide();
-// });
+// source: Spoonacular and ChatGPT
+function searchRecipes() {
+    const query = document.getElementById('query').value;
+    const diet = document.getElementById('diet').value;
+    const intolerances = document.getElementById('intolerances').value;
+    const maxReadyTime = document.getElementById('maxReadyTime').value;
 
-// // When a tab is clicked
-// $('.tab').click(function() {
-//   // Get the value of the data-tab attribute of the clicked tab
-//   var tabId = $(this).data('tab');
-  
-//   // Remove the 'active' class from all tabs
-//   $('.tab').removeClass('active');
-//   // Add the 'active' class to the clicked tab
-//   $(this).addClass('active');
-  
-//   // Hide all tab panes
-//   $('.tab-pane').removeClass('active');
-//   // Show the tab pane corresponding to the clicked tab
-//   $('[data-tab-content="' + tabId + '"]').addClass('active');
-// });
+    const apiKey = '01f947a8141b4c8cb4601915e6acf652'; 
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&diet=${diet}&intolerances=${intolerances}&maxReadyTime=${maxReadyTime}`;
+
+    console.log('API URL:', apiUrl);
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log('API Response:', data);
+
+            const recipes = data.results;
+            const resultsDiv = document.getElementById('recipeResults');
+
+            if (recipes && recipes.length > 0) {
+                // Use Promise.all to fetch details for all recipes
+                const fetchPromises = recipes.map(recipe => {
+                    const recipeId = recipe.id;
+                    const recipeDetailsUrl = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
+
+                    return fetch(recipeDetailsUrl)
+                        .then(response => response.json())
+                        .then(recipeDetails => {
+                            return { ...recipe, instructions: recipeDetails.instructions, ingredients: recipeDetails.extendedIngredients };
+                        });
+                });
+
+                Promise.all(fetchPromises)
+                    .then(recipesWithInstructions => {
+                        const recipeWithInstructions = recipesWithInstructions.find(recipe => recipe.instructions && recipe.ingredients);
+
+                        if (recipeWithInstructions) {
+                            const ingredientsList = recipeWithInstructions.ingredients.map(ingredient => `<li>${ingredient.original}</li>`).join('');
+
+                            const html = `
+                                <div class="recipe">
+                                    <h3>${recipeWithInstructions.title}</h3>
+                                    <h5>- "Yes, Chef!" -</h5>
+                                    <h4>Ingredients:</h4>
+                                    <ul>${ingredientsList}</ul>
+                                    <h4>Instructions:</h4>
+                                    <p>${recipeWithInstructions.instructions}</p>
+                                </div>`;
+                            resultsDiv.innerHTML = html;
+                        } else {
+                            resultsDiv.innerHTML = '<p>No results found with instructions and ingredients.</p>';
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error fetching recipe details:', error);
+                        resultsDiv.innerHTML = '<p>Error fetching recipe details.</p>';
+                    });
+            } else {
+                resultsDiv.innerHTML = '<p>No results found.</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            document.getElementById('recipeResults').innerHTML = 'Error fetching data';
+        });
+}
+
